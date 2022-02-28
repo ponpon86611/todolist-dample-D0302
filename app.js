52231646b0c0a28a2c18563de58671e33d92504e
Pon <blackdog755308@gmail.com>
2022-02-28 23:35:46 +0800

    增加環境設定-有關要佈署到heroku

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ const server = http.createServer((req,res)=>{
 
 });
 
-server.listen(PORT , ()=>{
+server.listen(process.env.PORT || PORT , ()=>{
     console.log(`server listen on port: ${PORT}`);
 });
